c96614220923acd5cd591f71f75e8eeac17502b7
NicoChlebo <nico.chlebo@gmail.com>
2026-03-27 15:15:36 +0100

    green test for cancellation requested 48h

--- src/cancelReservation.js
+++ src/cancelReservation.js
@@ -7,5 +7,12 @@ export function cancelReservation(id, existingReservations = [], now = new Date(
         throw new ServiceError("Reservation not found");
     }
 
+    const FORTY_EIGHT_HOURS_IN_MS = 48 * 60 * 60 * 1000;
+    const timeDifference = reservation.startDate.getTime() - now.getTime();
+
+    if (timeDifference < FORTY_EIGHT_HOURS_IN_MS) {
+        throw new ServiceError("Cannot cancel a reservation less than 48h before the start");
+    }
+
     return true;
 }
