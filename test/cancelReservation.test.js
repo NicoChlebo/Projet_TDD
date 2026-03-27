86f7b4848301d7852cce0271558080645aa1a7f6
NicoChlebo <nico.chlebo@gmail.com>
2026-03-27 15:12:42 +0100

    red test for cancellation requested 48h

--- test/cancelReservation.test.js
+++ test/cancelReservation.test.js
@@ -18,4 +18,19 @@ describe("Given i try to cancel a reservation", () => {
         expect(() => cancelReservation(unknownId, existingReservations, now))
             .toThrow(new ServiceError("Reservation not found"));
     });
+
+    test("When the cancellation is requested less than 48h before start", () => {
+        const now = new Date("2026-06-09T14:00:00Z");
+        const existingReservations = [
+            {
+                id: 1,
+                name: "Client 1",
+                startDate: new Date("2026-06-10T14:00:00Z"),
+                endDate: new Date("2026-06-12T14:00:00Z")
+            }
+        ];
+
+        expect(() => cancelReservation(1, existingReservations, now))
+            .toThrow(new ServiceError("Cannot cancel a reservation less than 48h before the start"));
+    });
 });
